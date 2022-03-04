bc4d75f72c3b22aa10cc855dcf3887cc26c0cf48
Joshua Wilkerson <joshua.wilkerson@gmail.com>
2022-03-03 23:20:58 -0500

    Implemented unique ID creation

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -85,8 +86,8 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        String uniqueID = UUID.randomUUID().toString();
+        return n + uniqueID;
     }
 
 
